6a8b68386ebd9661b0490c7dd485d90e11fdb534
DESKTOP-BF3UMEE\user <crlis@naver.com>
2022-02-23 21:47:44 +0900

    퀴즈 추가

--- src/main/java/com/quiz/lesson03/Lesson03Quiz02RestController.java
+++ src/main/java/com/quiz/lesson03/Lesson03Quiz02RestController.java
@@ -2,6 +2,7 @@ package com.quiz.lesson03;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson03.bo.RealEstateBO;
@@ -34,5 +35,27 @@ public class Lesson03Quiz02RestController {
 
 		return "입력 성공: " + row;
 	}
+	
+	// http://localhost/lesson03/quiz02/2?realtor_id=5
+	@RequestMapping("/lesson03/quiz02/2")
+	public String quiz02_2(
+			@RequestParam("realtor_id") int realtorId) {
+//		address : 썅떼빌리버 오피스텔 814호
+//		area : 45
+//		type : 월세
+//		price : 100000
+//		rentPrice : 120
+		
+//		int realtorId, String address, int area,
+//		String type, int price, Integer rentPrice
+		int row = realEstateBO.addRealEstateAsField(realtorId, "썅떼빌리버 오피스텔 814호", 45, "월세", 100000, 120);
+		
+		return "입력 성공" + row;
+		
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/com/quiz/lesson03/dao/RealEstateDAO.java
+++ src/main/java/com/quiz/lesson03/dao/RealEstateDAO.java
@@ -19,6 +19,23 @@ public interface RealEstateDAO {
 			@Param("price")int price); // @PAram은 파라미터들을 amp으로 구성해준다.
 	
 	public int insertRealEstate(RealEstate realEstate);
+	
+	public int insertRealEstateAsField(
+			@Param("realtorId") int realtorId,
+			@Param("address") String address, 
+			@Param("area") int area,
+			@Param("type") String type, 
+			@Param("price") int price, 
+			@Param("rentPrice") Integer rentPrice);
+	
+	public int updateRealEstateById(
+			@Param("id") int id,
+			@Param("type") String type,
+			@Param("price") int price);
+	
+	public int deleteRealEstateById(
+			@Param("id") int id);
+	
 }
 
 
